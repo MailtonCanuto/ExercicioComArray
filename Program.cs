48145a8271aedcc22ce824e024233fbe8fbe3975
MailtonCanuto <mgcfisio@hotmail.com>
2023-10-14 18:47:05 -0300

    Final Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orders = orderStream.Split(',');
+
+for (int i = 0; i < orders.Length; i++)
+{
+    char[] letters = orders[i].ToCharArray();
+
+    if (letters.Length == 4)
+    {
+        Console.WriteLine(orders[i]);
+    }
+    else
+    {
+        Console.WriteLine(orders[i] + "\t- Falso");
+    }
+}
